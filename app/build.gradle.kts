149116b9983996bdbc953407ae0e9233bf5b318b
Rhonal Alonso Delgado Padilla <rhonal.delgado@gmail.com>
2024-05-20 17:21:59 +0200

    Se agrega comentarios de explicacion del codigo y se limplia el codigo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.android.gms:play-services-base:18.3.0")
+    implementation ("com.google.android.gms:play-services-tflite-java:16.2.0-beta02")
 
     //Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
@@ -54,7 +56,6 @@ dependencies {
     implementation ("com.google.mlkit:image-labeling:17.0.8")
     implementation ("com.google.android.gms:play-services-mlkit-image-labeling:16.0.8")
 
-    implementation ("com.google.android.gms:play-services-base:18.3.0")
-    implementation ("com.google.android.gms:play-services-tflite-java:16.2.0-beta02")
+
 
 }
--- app/src/main/java/com/example/testmlandroid/MainActivity.kt
+++ app/src/main/java/com/example/testmlandroid/MainActivity.kt
@@ -1,121 +1,159 @@
 package com.example.testmlandroid
 
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.ImageView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import android.Manifest
-import android.app.Activity
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.YuvImage
+import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeler
 import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    // Componentes de la interfaz de usuario
     private lateinit var ivPicture: ImageView
     private lateinit var tvResult: TextView
     private lateinit var btnChoosePicture: Button
 
+    // Códigos de permisos
     private val CAMERA_PERMISSION_CODE = 123
     private val READ_MEDIA_IMAGE_PERMISSION_CODE = 113
     private val WRITE_STORAGE_PERMISSION_CODE = 113
 
-    private val TAG="Mi ML"
+    // Tag para logging
+    private val TAG = "Mi ML"
 
+    // Lanzadores de resultados de actividades
     private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
     private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
 
+    // Componentes de ML Kit
     private lateinit var inputImage: InputImage
+    private lateinit var imageLabeler: ImageLabeler
 
-    private lateinit var imagelabeler: ImageLabeler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicializar componentes de la interfaz de usuario
         ivPicture = findViewById(R.id.ivPicture)
         tvResult = findViewById(R.id.tvresult)
         btnChoosePicture = findViewById(R.id.btnChoosePicture)
 
-        //me permite variar el porcentaje de confianza del etiquetado
-        //en este caso que solo me acepte el porcentaje superior al 80%
-        val options = ImageLabelerOptions.Builder()
-            .setConfidenceThreshold(0.8f)
-            .build()
+        // Registrar lanzadores de resultados de actividades
+        setupActivityResultLaunchers()
 
-        imagelabeler = ImageLabeling.getClient(options)
-
-        cameraLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val data = result?.data
-            try {
-                val photo = data?.extras?.get("data") as Bitmap
-                ivPicture.setImageBitmap(photo)
-                inputImage = InputImage.fromBitmap(photo, 0)
-                processImage()
-            } catch (e: Exception) {
-                Log.d(TAG, "onActivityResult: ${e.message}")
-            }
-        }
-
-        galleryLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val data = result.data
-            try {
-                inputImage = InputImage.fromFilePath(this@MainActivity, data?.data!!)
-                ivPicture.setImageURI(data?.data)
-                processImage()
-            } catch (e: Exception) {
-
-            }
-        }
-
+        // Establecer listener de clic para el botón
         btnChoosePicture.setOnClickListener {
-            val options = arrayOf("Camara", "Galeria")
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Seleccione una opción")
-
-            builder.setItems(options, DialogInterface.OnClickListener{
-                dialog, which ->
-                if(which==0){
-                    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    cameraLauncher.launch(cameraIntent)
-                }else{
-                    val storageIntent = Intent()
-                    storageIntent.setType("image/*")
-                    storageIntent.setAction(Intent.ACTION_GET_CONTENT)
-                    galleryLauncher.launch(storageIntent)
-                }
-            })
-            builder.show()
+            showImageSourceDialog()
         }
     }
 
+    // Configurar los lanzadores de resultados de actividades
+    private fun setupActivityResultLaunchers() {
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            handleCameraResult(result)
+        }
+
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            handleGalleryResult(result)
+        }
+    }
+
+    // Manejar el resultado de la cámara
+    private fun handleCameraResult(result: ActivityResult?) {
+        val data = result?.data
+        try {
+            val photo = data?.extras?.get("data") as Bitmap
+            ivPicture.setImageBitmap(photo)
+            inputImage = InputImage.fromBitmap(photo, 0)
+            processImage()
+        } catch (e: Exception) {
+            Log.d(TAG, "onActivityResult: ${e.message}")
+        }
+    }
+
+    // Manejar el resultado de la galería
+    private fun handleGalleryResult(result: ActivityResult?) {
+        val data = result?.data
+        try {
+            inputImage = InputImage.fromFilePath(this@MainActivity, data?.data!!)
+            ivPicture.setImageURI(data?.data)
+            processImage()
+        } catch (e: Exception) {
+            Log.d(TAG, "handleGalleryResult: ${e.message}")
+        }
+    }
+
+    // Mostrar un diálogo para elegir la fuente de la imagen
+    private fun showImageSourceDialog() {
+        val options = arrayOf("Camara", "Galeria")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Seleccione una opción")
+        builder.setItems(options) { dialog, which ->
+            when (which) {
+                0 -> launchCamera()
+                1 -> launchGallery()
+            }
+        }
+        builder.show()
+    }
+
+    // Lanzar la intención de la cámara
+    private fun launchCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraLauncher.launch(cameraIntent)
+    }
+
+    // Lanzar la intención de la galería
+    private fun launchGallery() {
+        val storageIntent = Intent()
+        storageIntent.setType("image/*")
+        storageIntent.setAction(Intent.ACTION_GET_CONTENT)
+        galleryLauncher.launch(storageIntent)
+    }
+
+    // Procesar la imagen seleccionada con ML Kit
     private fun processImage() {
-        imagelabeler.process(inputImage)
-            .addOnSuccessListener {
+        // 1. Configurar opciones del etiquetador de imágenes
+        //me permite variar el porcentaje de confianza del etiquetado
+        //en este caso que solo me acepte el porcentaje superior al 60%
+        val options = ImageLabelerOptions.Builder()
+            .setConfidenceThreshold(0.6f)
+            .build()
+        //2. Crea una instancia del etiquetador de imagenes con las opciones configuradas anteriormente
+        imageLabeler = ImageLabeling.getClient(options)
+
+        //3. Procesamiento de la imagen
+        imageLabeler.process(inputImage)
+            .addOnSuccessListener { labels ->
+                //si el procesamiento es exitoso, recorrer las etiquetas generadas
                 var result = ""
-                for (label in it) {
-                    result = result + "\n"+ label.text + " -> " + label.confidence * 100 + "%"
+                for (label in labels) {
+                    //concatenamos el texto de la etiqueta y su nivel de confianza en porcentaje
+                    result += "\n${label.text} -> ${label.confidence * 100}%"
                 }
+                //mostramos resultados
                 tvResult.text = result
-            }.addOnFailureListener {
-                Log.d(TAG, "procesando Imagen: ${it.message}")
+            }
+            .addOnFailureListener { e ->
+                //si el proceso falla, registrar el error en los logs
+                Log.d(TAG, "procesando Imagen: ${e.message}")
             }
     }
 
@@ -124,37 +162,35 @@ class MainActivity : AppCompatActivity() {
         checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
     }
 
+    // Verificar y solicitar los permisos necesarios
     private fun checkPermission(permission: String, requestCode: Int) {
-        if (ContextCompat.checkSelfPermission(this, permission)
-            == PackageManager.PERMISSION_DENIED) {
-           ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
-            } else {
-                // Permiso denegado
-                Toast.makeText(this, "CAMARA Permisos denegado", Toast.LENGTH_SHORT).show()
+        when (requestCode) {
+            CAMERA_PERMISSION_CODE -> {
+                handlePermissionResult(grantResults, "CAMARA")
             }
-        } else if(requestCode == READ_MEDIA_IMAGE_PERMISSION_CODE){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                checkPermission(Manifest.permission.READ_MEDIA_IMAGES, READ_MEDIA_IMAGE_PERMISSION_CODE)
-            } else {
-                // Permiso denegado
-                Toast.makeText(this, "GALERIA Permisos denegado", Toast.LENGTH_SHORT).show()
+            READ_MEDIA_IMAGE_PERMISSION_CODE -> {
+                handlePermissionResult(grantResults, "GALERIA")
             }
-        } else if(requestCode == WRITE_STORAGE_PERMISSION_CODE){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, WRITE_STORAGE_PERMISSION_CODE)
-            } else {
-                // Permiso denegado
-                Toast.makeText(this, "ESCRIBIR Permisos denegado", Toast.LENGTH_SHORT).show()
+            WRITE_STORAGE_PERMISSION_CODE -> {
+                handlePermissionResult(grantResults, "ESCRIBIR")
             }
+        }
+    }
+
+    // Manejar el resultado de la solicitud de permisos
+    private fun handlePermissionResult(grantResults: IntArray, permissionName: String) {
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+        } else {
+            Toast.makeText(this, "$permissionName Permisos denegado", Toast.LENGTH_SHORT).show()
         }
     }
 }
